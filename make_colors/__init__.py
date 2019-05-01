110072eb3af929ac03f62455edbf89ef75166be7
tester001 <todut001@gmail.com>
2019-05-01 20:35:36 +0700

    version: 3.15 ~ 2019-05-01 20:35:28:852000

--- make_colors/__init__.py
+++ make_colors/__init__.py
@@ -1,1 +1,7 @@
 from .make_colors import make_colors
+
+import imp
+version = imp.load_source('version', "../__version__.py")
+__version__ 	= version.version
+__email__		= "licface@yahoo.com"
+__author__		= "licface@yahoo.com"
--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ import io
 import re
 from setuptools import setup
 
+import os
+import shutil
+try:
+    os.remove(os.path.join('make_colors', '__version__.py'))
+except:
+    pass
+shutil.copy2('__version__.py', 'make_colors')
+
 with io.open("README.rst", "rt", encoding="utf8") as f:
     readme = f.read()
 
